96aa9f3a52847af5cff537c51f632ac1a0596349
Perrine <perrine.morsch@gmail.com>
2023-07-03 15:14:17 +0200

    optimisation des images

--- config/init_subjects.php
+++ config/init_subjects.php
@@ -13,7 +13,7 @@ require_once(__DIR__ . '/../models/Subject.php');
 
 $main = new Subject;
 $main->title = 'Accueil';
-$main->img = '/../public/assets/img/subjects/main.jpg';
+$main->img = '/../public/assets/img/subjects/main.webp';
 $main->meta_description = 'Découvrez toute l\'actualité des jeux vidéo du moment ! Les prochaines sorties, les derniers tests et plus encore !';
 $main->link = ['https://www.jeuxactu.com/rss/ja.rss', 'https://www.jeuxactu.com/rss/tests.rss', 'https://www.jeuxactu.com/rss/sorties.rss'];
 
@@ -25,7 +25,7 @@ $main->link = ['https://www.jeuxactu.com/rss/ja.rss', 'https://www.jeuxactu.com/
 
 $pc = new Subject;
 $pc->title = 'PC';
-$pc->img = '/../public/assets/img/subjects/pc.jpg';
+$pc->img = '/../public/assets/img/subjects/pc.webp';
 $pc->meta_description = 'Découvrez toute l\'actualité des jeux PC du moment !';
 $pc->link = 'https://www.jeuxactu.com/rss/pc.rss';
 
@@ -33,7 +33,7 @@ $pc->link = 'https://www.jeuxactu.com/rss/pc.rss';
 
 $xbox = new Subject;
 $xbox->title = 'XBOX';
-$xbox->img = '/../public/assets/img/subjects/xbox.jpg';
+$xbox->img = '/../public/assets/img/subjects/xbox.webp';
 $xbox->meta_description = 'Découvrez toute l\'actualité des jeux XBOX du moment !';
 $xbox->link = 'https://www.jeuxactu.com/rss/xbox-series-x.rss';
 
@@ -41,7 +41,7 @@ $xbox->link = 'https://www.jeuxactu.com/rss/xbox-series-x.rss';
 
 $ps5 = new Subject;
 $ps5->title = 'PS5';
-$ps5->img = '/../public/assets/img/subjects/ps5.jpg';
+$ps5->img = '/../public/assets/img/subjects/ps5.webp';
 $ps5->meta_description = 'Découvrez toute l\'actualité des jeux PS5 du moment !';
 $ps5->link = 'https://www.jeuxactu.com/rss/ps5.rss';
 
@@ -57,7 +57,7 @@ $switch->link = 'https://www.jeuxactu.com/rss/switch.rss';
 
 $mobile = new Subject;
 $mobile->title = 'Mobile';
-$mobile->img = '/../public/assets/img/subjects/mobile.jpg';
+$mobile->img = '/../public/assets/img/subjects/mobile.webp';
 $mobile->meta_description = 'Découvrez toute l\'actualité des jeux sur Mobile du moment !';
 $mobile->link = 'https://www.jeuxactu.com/rss/mobile.rss';
 
